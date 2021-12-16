d3d4d0b0166d313cea78cb8ba8b145bd9345fd90
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-12-17 02:57:24 +0300

    Add genre id dispatch

--- src/com/umut/videostream/model/repository/MovieTMDBRepository.java
+++ src/com/umut/videostream/model/repository/MovieTMDBRepository.java
@@ -25,4 +25,17 @@ public class MovieTMDBRepository implements IMovieRepository{
         return MessageFormat.format("{0}w{1}/{2}",IMG_BASE_URL ,width, posterPath);
     }
 
+    // Implementation of API's id values
+    // https://api.themoviedb.org/3/genre/movie/list?api_key={{your_api_key}}
+    private int getGenreId(EMovieGenre genre){
+        return switch (genre){
+            case ACTION -> 28;
+            case COMEDY -> 35;
+            case DOCUMENTARY -> 99;
+            case DRAMA -> 18;
+            case HISTORY -> 36;
+            case HORROR -> 27;
+            case MUSIC -> 10402;
+        };
+    }
 }
